6514a9b2c80360b9047b421597124d0935febf57
2Hex <rxgamerhere@gmail.com>
2022-01-03 10:54:07 +0100

    Attempt1

---
+++ src/main/java/me/hex/searcher/Main.java
@@ -0,0 +1,40 @@
+package me.hex.searcher;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiChest;
+import net.minecraft.inventory.ContainerChest;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Main.MODID, version = Main.VERSION)
+public class Main {
+    public static final String MODID = "searcher";
+    public static final String VERSION = "1.0";
+    public static Minecraft mc;
+
+    public static String searchBy = "";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        mc = Minecraft.getMinecraft();
+    }
+
+    @EventHandler
+    public void preinit(FMLPreInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new OpenInventoryEvent());
+    }
+
+    public static String getGUIName(GuiScreen gui) {
+        if (gui instanceof GuiChest) {
+            GuiChest chest = (GuiChest) gui;
+            return ((ContainerChest) chest.inventorySlots)
+                    .getLowerChestInventory().getDisplayName().getUnformattedText();
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/me/hex/searcher/OpenInventoryEvent.java
@@ -0,0 +1,266 @@
+package me.hex.searcher;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.gui.inventory.GuiChest;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.input.Keyboard;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static me.hex.searcher.Main.mc;
+
+public class OpenInventoryEvent {
+    public final Gson gson;
+
+    static boolean notFirst = false;
+    ArrayList<Slot> slots;
+    HashMap<ItemStack, Slot> itemSlot;
+
+
+    public OpenInventoryEvent() {
+        gson = new GsonBuilder().setPrettyPrinting().create();
+        itemSlot = new HashMap<ItemStack, Slot>();
+        slots = new ArrayList<Slot>();
+
+    }
+
+    @SubscribeEvent
+    public void onRenderGuiScreen(GuiScreenEvent.DrawScreenEvent.Post event) {
+
+        GuiScreen gui = event.gui;
+        String guiName = Main.getGUIName(gui);
+
+        if (guiName == null) {
+
+            return;
+        }
+        if (!(gui instanceof GuiChest)) {
+
+            return;
+        }
+
+            if (!notFirst)
+                mc.displayGuiScreen(new SearchGui(event.gui));
+
+            slots.clear();
+
+        for (Slot slot : ((GuiChest) gui).inventorySlots.inventorySlots) {
+
+            if (slot.getHasStack()) {
+                slots.add(slot);
+                renderItemReturn(((GuiChest) gui), slot.getStack(), slot.xDisplayPosition, slot.yDisplayPosition);
+            }
+            if(slots.contains(slot) && ((GuiChest) gui).inventorySlots
+                    .inventoryItemStacks.lastIndexOf(slot.getStack()) >= 1){
+                renderItemReturn(((GuiChest) gui), slot.getStack(), slot.xDisplayPosition, slot.yDisplayPosition);
+            }
+        }
+
+        slots.clear();
+    }
+
+    public void renderItemReturn(GuiChest gui, ItemStack stack, int x, int y) {
+
+        if (stack != null && !(stack.stackSize >= 1)) return;
+
+        boolean matches;
+
+        if (Main.searchBy.trim().isEmpty()) {
+            matches = true;
+        } else {
+                matches = doesStackMatchSearch(stack, Main.searchBy.trim());
+        }
+        if (!matches) {
+
+            GlStateManager.pushMatrix();
+            GlStateManager.translate(0, 0, 110 + Minecraft.getMinecraft().getRenderItem().zLevel);
+            final int overlayColourDark = new Color(0, 0, 0, 120).getRGB();
+            int size = gui.inventorySlots.inventorySlots.size();
+            drawOnSlot(size, x, y, overlayColourDark);
+
+        }
+    }
+    /*
+    ------------------------------------
+    method taken from DSM's SkyblockMOD
+    ------------------------------------
+     */
+    public static void drawOnSlot(int size, int xSlotPos, int ySlotPos, int colour) {
+
+        ScaledResolution sr = new ScaledResolution(Minecraft.getMinecraft());
+
+        int guiLeft = (sr.getScaledWidth() - 176) / 2;
+        int guiTop = (sr.getScaledHeight() - 222) / 2;
+        int x = guiLeft + xSlotPos;
+        int y = guiTop + ySlotPos;
+        // Move down when chest isn't 6 rows
+        if (size != 90) y += (6 - (size - 36) / 9) * 9;
+
+        GlStateManager.translate(0, 0, 1);
+        Gui.drawRect(x, y, x + 16, y + 16, colour);
+        GlStateManager.popMatrix();
+
+    }
+        /*
+        ---------------------------------------------------------------
+        methods from here and below are taken from Moulberry's NEU Mod.
+        ---------------------------------------------------------------
+        */
+    public boolean doesStackMatchSearch(ItemStack stack, String query) {
+
+        if (query.startsWith("title:")) {
+            query = query.substring(6);
+            return searchString(stack.getDisplayName(), query);
+        } else if (query.startsWith("desc:")) {
+            query = query.substring(5);
+            String lore = "";
+            NBTTagCompound tag = stack.getTagCompound();
+            if (tag != null) {
+                NBTTagCompound display = tag.getCompoundTag("display");
+                if (display.hasKey("Lore", 9)) {
+                    NBTTagList list = display.getTagList("Lore", 8);
+                    for (int i = 0; i < list.tagCount(); i++) {
+                        lore += list.getStringTagAt(i) + " ";
+                    }
+                }
+            }
+            return searchString(lore, query);
+        } else if (query.startsWith("id:")) {
+            query = query.substring(3);
+            String internalName = getInternalNameForItem(stack);
+            return query.equalsIgnoreCase(internalName);
+        } else {
+            boolean result = false;
+            if (!query.trim().contains(" ")) {
+                StringBuilder sb = new StringBuilder();
+                for (char c : query.toCharArray()) {
+                    sb.append(c).append(" ");
+                }
+                result = searchString(stack.getDisplayName(), sb.toString());
+            }
+            result = result || searchString(stack.getDisplayName(), query);
+
+            String lore = "";
+            NBTTagCompound tag = stack.getTagCompound();
+            if (tag != null) {
+                NBTTagCompound display = tag.getCompoundTag("display");
+                if (display.hasKey("Lore", 9)) {
+                    NBTTagList list = display.getTagList("Lore", 8);
+                    for (int i = 0; i < list.tagCount(); i++) {
+                        lore += list.getStringTagAt(i) + " ";
+                    }
+                }
+            }
+
+            result = result || searchString(lore, query);
+
+            return result;
+        }
+    }
+
+
+    public boolean searchString(String toSearch, String query) {
+
+        int lastMatch = -1;
+
+        toSearch = clean(toSearch).toLowerCase();
+        query = clean(query).toLowerCase();
+        String[] splitToSearch = toSearch.split(" ");
+        out:
+        for (String s : query.split(" ")) {
+            for (int i = 0; i < splitToSearch.length; i++) {
+                if (lastMatch == -1 || lastMatch == i - 1) {
+                    if (splitToSearch[i].startsWith(s)) {
+                        lastMatch = i;
+                        continue out;
+                    }
+                }
+            }
+            return false;
+        }
+
+        return true;
+    }
+
+    private String clean(String str) {
+        return str.replaceAll("(\u00a7.)|[^0-9a-zA-Z ]", "").toLowerCase().trim();
+    }
+
+    public String getInternalNameForItem(ItemStack stack) {
+
+        if (stack == null) return null;
+        NBTTagCompound tag = stack.getTagCompound();
+        return getInternalnameFromNBT(tag);
+    }
+
+    public String getInternalnameFromNBT(NBTTagCompound tag) {
+
+        String internalname = null;
+        if (tag != null && tag.hasKey("ExtraAttributes", 10)) {
+            NBTTagCompound ea = tag.getCompoundTag("ExtraAttributes");
+
+            if (ea.hasKey("id", 8)) {
+                internalname = ea.getString("id").replaceAll(":", "-");
+            } else {
+                return null;
+            }
+
+            if ("PET".equals(internalname)) {
+                String petInfo = ea.getString("petInfo");
+                if (petInfo.length() > 0) {
+                    JsonObject petInfoObject = gson.fromJson(petInfo, JsonObject.class);
+                    internalname = petInfoObject.get("type").getAsString();
+                    String tier = petInfoObject.get("tier").getAsString();
+                    if ("COMMON".equals(tier)) {
+                        internalname += ";0";
+                    } else if ("UNCOMMON".equals(tier)) {
+                        internalname += ";1";
+                    } else if ("RARE".equals(tier)) {
+                        internalname += ";2";
+                    } else if ("EPIC".equals(tier)) {
+                        internalname += ";3";
+                    } else if ("LEGENDARY".equals(tier)) {
+                        internalname += ";4";
+                    } else if ("MYTHIC".equals(tier)) {
+                        internalname += ";5";
+                    }
+                }
+            }
+            if ("ENCHANTED_BOOK".equals(internalname)) {
+                NBTTagCompound enchants = ea.getCompoundTag("enchantments");
+
+                for (String enchname : enchants.getKeySet()) {
+                    internalname = enchname.toUpperCase() + ";" + enchants.getInteger(enchname);
+                    break;
+                }
+            }
+        }
+
+        return internalname;
+    }
+
+    @SubscribeEvent
+    public void keyBoardInput(GuiScreenEvent.KeyboardInputEvent event) {
+
+        if (Keyboard.getEventKey() == 24) {
+            if (event.gui instanceof GuiChest) {
+                mc.displayGuiScreen(new SearchGui(event.gui));
+            }
+        }
+    }
+
+}
